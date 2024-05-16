2117e43ce23bf4681c4fd5027f121f191323ab1d
Malli Gelli <malleswari.gelli@gmail.com>
2024-05-16 09:05:49 -0500

    setup file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Medical Chatbot",
+    version="0.0.0",
+    author="Malleswari Gelli",
+    author_email="malleswari.gelli@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
